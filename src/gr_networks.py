ef11189bafc086fa239b236e3bae0a3d11403a50
Andre Rendeiro <afrendeiro@gmail.com>
2015-06-26 16:10:20 +0200

    add more GRN ideas

--- src/gr_networks.py
+++ src/gr_networks.py
@@ -20,18 +20,34 @@ samples = [s for s in prj.samples if type(s) == ATACseqSample]
 
 
 # FOOTPRINTING
+# - Go to each TF motif site annotated: http://compbio.mit.edu/encode-motifs/
+# - get window around (e.g. +/- 100bp), footprint with CENTIPEDE or PIQ
+# - save posterior probabilities for every site
+
 # ideally with PIQ (seems to be the best performing method)
 
+# more:
 # get factor occupancy score (FOS) for each factor in each site
 # cluster again patients based on TF occupancy
 
 
 # NETWORKS
 # Network construction:
-# - after footprinting
-# - assign a confident TF footprint to its target gene:
-#     - is it in a promoter? is it in a known enhancer of a gene?
-# - build network of A -> B based on this
+
+# Calculate a "regulation score" between EVERY TF and EVERY gene:
+# - for each TF, for each chromossome, sum the weighted posterior probabilities of factor A regulating gene I.
+# weigh these by dividing by the (log) of the distance of each binding site to the TSS of gene I.
+# This will give a matrix of TS-gene "regulation scores".
+# Investigate the distribution of scores.
+
+# Compare regulation across patients:
+# - for each TF, correlate the scores with the scores of all other transcription factors in the other patient.
+
+# Additionally
+# - build network of TF -> gene based on:
+# 	- binding at the promoter or at known enhancers for the gene
+#   OR
+#   - consider regulation only if score is above threshold
 
 # Network types:
 # patient-specific:
