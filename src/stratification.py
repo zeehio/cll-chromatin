525a137ea6dc15b7a9cae929704bc3575dbe0032
Andre Rendeiro <afrendeiro@gmail.com>
2015-07-11 14:38:15 +0200

    calculating RPKMs in peak sites

--- src/stratification.py
+++ src/stratification.py
@@ -5,6 +5,33 @@ import os
 from pipelines import Project, ATACseqSample
 import pybedtools
 from collections import Counter
+import pysam
+
+
+def normalizeByIntervalLength(series):
+    """
+    Divides a pandas.Series with numerical values by the length of the interval
+    (defined in "chr", "start", "end") in a bam file (RPK).
+    """
+    length = float(series["end"] - series["start"])
+
+    rpK = (series.drop(["chr", "start", "end", "name", "score", "strand"]) / length) * 1e3
+
+    return series[["chr", "start", "end", "name", "score", "strand"]].append(rpK)
+
+
+def normalizeByLibrarySize(series, samples):
+    """
+    Divides a pandas.Series with numerical values by the total number of mapped
+    reads in a bam file.
+    """
+    # get sample which this series belongs to
+    sample = [sample for sample in samples if sample.name == series.name]
+
+    # get number of aligned reads for that sample
+    size = float(pysam.AlignmentFile(sample.filtered).mapped)
+
+    return (series / size) * 1e6
 
 
 # Read configuration file
@@ -22,15 +49,20 @@ samples = [s for s in prj.samples if type(s) == ATACseqSample]
 
 
 # GET CONSENSUS SITES ACROSS SAMPLES
-# Get union of all sample's peaks
-# with merged ovelaping intervals
 for i, sample in enumerate(samples):
+    # Get summits of peaks and window around them
+    peaks = pybedtools.BedTool(sample.filteredPeaks).slop(g=pybedtools.chromsizes('hg19'), b=250)
+    # Merge overlaping peaks within a sample
+    peaks = peaks.merge()
     if i == 0:
-        sites = pybedtools.BedTool(sample.filteredPeaks)
+        sites = peaks
     else:
-        sites = sites.cat(pybedtools.BedTool(sample.filteredPeaks))
+        # Concatenate all peaks
+        sites = sites.cat(peaks)
 
-sites.saveas(os.path.join(dataDir, "peaks_concatenated.bed"))
+# Merge overlaping peaks across samples
+sites = sites.merge()
+sites.saveas(os.path.join(dataDir, "all_sample_peaks.concatenated.bed"))
 
 # Loop at summary statistics:
 # interval lengths, calculate support (number of samples overlaping each final one) etc...
@@ -41,19 +73,22 @@ support = sites.multi_intersect(i=[sample.filteredPeaks for sample in samples])
 # Remove some regions chtMT, chr.*random
 
 # After inspecting phenotypes, consider excluding more regions/chromosomes across all samples
-
+# e.g. chr11q, chr12
 
 # GET MEASURE OF CHROMATIN OPENNESS PER SITE PER PATIENT
 # Get read counts for each sample in the union of the sites
-# maybe consider enlarging a bit the sites (slop)
+coverage = sites.multi_bam_coverage(bams=[sample.filtered for sample in samples], p=True, q=30, D=True)
+# make dataframe
+sampleNames = [sample.name for sample in samples]
+coverage = coverage.to_dataframe(names=["chr", "start", "end", "name", "score", "strand"] + sampleNames)
+coverage.to_csv(os.path.join(dataDir, "all_sample_peaks.concatenated.raw_coverage.bed"), sep="\t", index=False)
 
-# e.g.:
-# bedtools annotate -names [sample.name for sample in samples] -i consensus_peaks.bed -files [sample.filtered for sample in samples]
-coverage = sites.multi_bam_coverage([sample.filtered for sample in samples], p=True, q=30)
-coverage.saveas(os.path.join(dataDir, "peaks_concatenated.coverage.bed"))
+# Divide by feature length
+coverage.apply(normalizeByIntervalLength, axis=1)
+# Divide by library size (mapped reads)
+coverage.apply(normalizeByLibrarySize, args=(samples, ), axis=0)
 
-# normalize counts by library size
-# e.g. try using DESeq2 sizeFactors() to get resizing factors
+coverage.saveas(os.path.join(dataDir, "all_sample_peaks.concatenated.fpkm.bed"))
 
 
 # GLOBAL CHARACTERIZATION
