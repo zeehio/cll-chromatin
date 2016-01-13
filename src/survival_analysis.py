1ac716e1bc380a22ff70dced71ea67729630b293
Andre Rendeiro <afrendeiro@gmail.com>
2016-01-13 11:12:01 +0100

    better survival analysis; predicted survival and hazard now exported to main analysis

--- src/survival_analysis.py
+++ src/survival_analysis.py
@@ -19,6 +19,7 @@ import re
 from lifelines import AalenAdditiveFitter
 import patsy
 from lifelines.utils import k_fold_cross_validation
+from joblib import Parallel, delayed
 
 
 # Set settings
@@ -83,6 +84,18 @@ def survival_plot(clinical, fitter, fitter_name, feature, time):
     ax.set_title("%s - %s since %s" % (feature, fitter_name, name))
     plt.savefig(os.path.join(plots_dir, "survival", "%s_%s_since_%s.svg" % (feature, fitter_name, name)), bbox_inches="tight")
     plt.close("all")
+
+
+def test_model(model):
+    X = patsy.dmatrix(model + " -1", clinical, return_type="dataframe")
+    X['T'] = [i.days / float(30) for i in clinical["duration_following"].ix[X.index]]
+    X['E'] = [True if i is not pd.np.nan else False for i in clinical["patient_death_date"].ix[X.index]]
+
+    r = list()
+    for penalty in range(1, 11):
+        aaf = AalenAdditiveFitter(coef_penalizer=penalty, fit_intercept=True)
+        r.append((penalty, k_fold_cross_validation(aaf, X, 'T', event_col='E', k=5)))
+    return r
 
 
 #
@@ -157,34 +170,21 @@ for name, time in times.items():
 [features.pop(features.index(i)) for i in ['SF3B1', 'ATM', 'NOTCH1', 'BIRC3', 'BCL2', 'MYD88', 'CHD2', 'NFKIE']]
 combinations = [" + ".join(j) for i in range(1, len(features) + 1) for j in itertools.combinations(features, i)]
 
-models = dict(zip(
+# Test all models
+performances = Parallel(n_jobs=-1)(delayed(test_model)(model) for model in combinations)
+
+validated = dict(zip(
     [re.sub(r" \+ ", "-", c) for c in combinations],
-    combinations
-))
-
-validated = dict()
-for model_name, model in models.items():
-    # Create matrix
-    X = patsy.dmatrix(model + " -1", clinical, return_type="dataframe")
-
-    # Add the survival data
-    # and since patsy removes rows with nan in variables used for matrix,
-    # we have to remove those as well from the survival data (fortunately original index is kept in X yay!)
-    X['T'] = [i.days / float(30) for i in clinical["duration_following"].ix[X.index]]
-    X['E'] = [True if i is not pd.np.nan else False for i in clinical["patient_death_date"].ix[X.index]]
-
-    for penalty in range(1, 11):
-        aaf = AalenAdditiveFitter(coef_penalizer=penalty, fit_intercept=True)
-        scores = k_fold_cross_validation(aaf, X, 'T', event_col='E', k=5)  # len(X) - 1)#, predictor_kwargs={"show_progress": False})
-        validated[(model_name, penalty)] = scores
-        # print(model, penalty, np.mean(scores), np.std(scores))
-
+    sum(performances, []))
+)
 
 # model concordance distribution
 fig, axis = plt.subplots(3, sharex=False, sharey=False, figsize=(15, 8))
-sns.distplot([np.mean(x[1]) for x in validated.items()], ax=axis[0])
-axis[1].scatter([len(x[0].split("-")) for x in validated.keys()], [np.mean(x[1]) for x in validated.items()])
-axis[2].scatter([x[1] for x in validated.keys()], [np.mean(x[1]) for x in validated.items()])
+sns.distplot([np.mean(x[1]) for x in validated.values()], ax=axis[0])
+# number of elements in model
+axis[1].scatter([len(x.split("-")) for x in validated.keys()], [np.mean(x[1]) for x in validated.values()])
+# penalty per model
+axis[2].scatter([x[0] for x in validated.values()], [np.mean(x[1]) for x in validated.values()])
 axis[0].set_xlabel("Mean model concordance")
 axis[0].set_ylabel("Density")
 axis[1].set_xlabel("Number of model terms")
@@ -195,38 +195,14 @@ fig.savefig(os.path.join(plots_dir, "survival", "model_selction_performance.svg"
 
 
 # sort by performance
-best_models = sorted(validated.items(), key=lambda x: np.mean(x[1]))
-
-# get best model
-model = models[best_models[-1][0][0]]
-penalty = best_models[-1][0][1]
-
-# regress with best model
-X = patsy.dmatrix(model + " -1", clinical, return_type="dataframe")
-X['T'] = [i.days / float(30) for i in clinical["duration_following"].ix[X.index]]
-X['E'] = [True if i is not pd.np.nan else False for i in clinical["patient_death_date"].ix[X.index]]
-
-aaf = AalenAdditiveFitter(coef_penalizer=penalty, fit_intercept=True)
-aaf.fit(X, 'T', event_col='E')
-
-
-# predict survival and hazard for all patients
-fig, axis = plt.subplots(2, sharex=True, sharey=False)
-aaf.predict_survival_function(X).plot(ax=axis[0], legend=False)
-aaf.predict_cumulative_hazard(X).plot(ax=axis[1], legend=False)
-axis[0].set_title("Best model - prediction for all patients")
-axis[1].set_xlabel("Time since diagnosis")
-axis[0].set_ylabel("Survival")
-axis[1].set_ylabel("Hazard")
-fig.savefig(os.path.join(plots_dir, "survival", "best_model_predictions_all_patients.svg"), bbox_inches="tight")
-
+best_models = sorted(validated.items(), key=lambda x: np.mean(x[1][1]))
 
 # Plot 5 best models
 fig, axis = plt.subplots(2, 5, sharex=False, sharey=False, figsize=(40, 15))
 for m in range(1, 6):
     # get best model
-    model = models[best_models[-m][0][0]]
-    penalty = best_models[-m][0][1]
+    model = re.sub("-", " + ", best_models[-m][0])
+    penalty = best_models[-m][1][0]
 
     # regress with best model
     X = patsy.dmatrix(model + " -1", clinical, return_type="dataframe")
@@ -244,6 +220,54 @@ for m in range(1, 6):
     # [i[m - 1].legend_.remove() for i in axis]
 fig.savefig(os.path.join(plots_dir, "survival", "best_5models_predictions_all_patients.svg"), bbox_inches="tight")
 
+# get best model
+model = re.sub("-", " + ", best_models[-1][0])
+penalty = best_models[-1][1][0]
 
-# Predict survival probability and hazard of each sample given its time since diagnosis
+# regress with best model
+X = patsy.dmatrix(model + " -1", clinical, return_type="dataframe")
+X['T'] = [i.days / float(30) for i in clinical["duration_following"].ix[X.index]]
+X['E'] = [True if i is not pd.np.nan else False for i in clinical["patient_death_date"].ix[X.index]]
+
+aaf = AalenAdditiveFitter(coef_penalizer=penalty, fit_intercept=True)
+aaf.fit(X, 'T', event_col='E')
+
+# predict survival and hazard for all patients
+fig, axis = plt.subplots(2, sharex=True, sharey=False)
+aaf.predict_survival_function(X).plot(ax=axis[0], legend=False)
+aaf.predict_cumulative_hazard(X).plot(ax=axis[1], legend=False)
+axis[0].set_title("Best model - prediction for all patients")
+axis[1].set_xlabel("Time since diagnosis")
+axis[0].set_ylabel("Survival")
+axis[1].set_ylabel("Hazard")
+fig.savefig(os.path.join(plots_dir, "survival", "best_model_predictions_all_patients.svg"), bbox_inches="tight")
+
+
+# Predict survival probability and hazard of each sample
+# at the time of sample collection
 # based on best model
+
+survival = aaf.predict_survival_function(X)
+hazard = aaf.predict_cumulative_hazard(X)
+
+clinical["duration_collection"] = clinical["sample_collection_date"] - clinical["diagnosis_date"]
+
+s = list()
+h = list()
+for i in clinical.index:
+    if i in survival.columns:
+        t = clinical.ix[i]["duration_collection"]
+        if t is not pd.NaT:
+            s.append(np.interp(t.days, survival.index, survival[i]))
+            h.append(np.interp(t.days, hazard.index, hazard[i]))
+            continue
+    s.append(np.nan)
+    h.append(np.nan)
+
+clinical['predicted_survival'] = s
+clinical['predicted_hazard'] = h
+
+# export
+clinical[["patient_id", "sample_id", "predicted_survival", "predicted_hazard"]].to_csv(
+    os.path.join("data", "survival_hazard_predictions.csv")
+)
